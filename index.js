a95178ac537eef4a732f769ddb60731df9f66a43
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-04-25 15:39:54 -0700

    commit tree 914ef33d11d8cad792f8f0acbbe3af1fbc102dea

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair = "*"){
+    return function(adjective = "special"){
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
+
+const Calculator = {add, subtract, multiply, divide}
+
+function add(num1, num2){
+   return num1 + num2
+}
+
+function subtract(num1, num2){
+    return num1 - num2
+ }
+
+ function multiply(num1, num2){
+    return num1 * num2
+ }
+
+ function divide(num1, num2){
+    return num1 / num2
+ }
+
+ function actionApplyer(num, array){
+    if(array.length === 0){
+        return num
+    }else{
+        array.forEach(element => {
+            num = element(num)
+        });
+        return num
+    }
+ }
